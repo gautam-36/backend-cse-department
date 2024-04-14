06137eea53c54d8231795fc3a69150871718e37e
gautam-36 <gautamkumarg660@gmail.con>
2024-04-14 19:30:13 +0530

    changed names of files

--- utils/authicated.js
+++ utils/authicated.js
@@ -1,17 +1,18 @@
-const jwt = require("jsonwebtoken")
 
-const Authenticated = async (req, res, next) => {
-    const Token = req.cookies.Acesstoken
-    console.log(Token);
+const jwt=require("jsonwebtoken")
 
-    if (!Token) {
+const Authenticated=async(req,res,next)=>{
+     const Token=req.cookies.Acesstoken
+     console.log(Token);
+
+     if(!Token){
         throw new Error("Token Does not exist");
-    }
+     }
 
-    const data = await jwt.verify(Token, process.env.ACCESS_TOKEN_SECRET)
-    console.log(data)
-    req.id = data._id;
-    next()
+      const data=await jwt.verify(Token,process.env.ACCESS_TOKEN_SECRET)
+      console.log(data)
+      req.id=data._id;
+      next()
 }
 
-module.exports = Authenticated
+module.exports=Authenticated
