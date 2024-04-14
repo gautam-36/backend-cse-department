1be8bd5987b7c3f5a6c6cf1c71df2a3e97a54e38
Kapildagar <91322878+Kapildagar@users.noreply.github.com>
2024-04-14 15:22:24 +0530

    added auth route

--- server.js
+++ server.js
@@ -10,13 +10,20 @@ const PORT = process.env.PORT || 5000;
 const uploadRoutes = require('./routes/uploadRoutes');
 const alumniRoutes = require('./routes/alumni');
 const facultyRoutes = require('./routes/faculty');
-
+const Authroute=require('./routes/Auth');
+const cookieParser = require('cookie-parser');
 
 
 // Middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Specify the origin(s) you want to allow
+  methods: ['GET', 'POST'], // Specify which methods are allowed
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true 
+}));
+app.use(cookieParser());
 
 
 // Database connection
@@ -30,6 +37,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use('/api', uploadRoutes);
 app.use('/api', alumniRoutes);
 app.use('/api', facultyRoutes);
+app.use('/api',Authroute);
 
 
 
